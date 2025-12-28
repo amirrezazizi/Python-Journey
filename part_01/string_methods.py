9aa6a0a76c4bd047c9a656538f0bd9a51c51e4f0
amirrezazizi <newara512@gmail.com>
2025-12-28 16:56:13 +0330

    add string_methods

---
+++ part_01/string_methods.py
@@ -0,0 +1,18 @@
+#this code show methods in str class and their functionality
+
+name='Amir'
+print(type(name))
+
+likes="dog and music and coding and coffe and apple and linux"
+res=likes.split("and")
+print(res)
+
+name.upper()
+print(name)
+name.lower()
+print(name)
+print("Hello".islower())
+print("HELLO".isupper())
+
+print(name.index('m'))
+#print(name.index('R'))           >>>> this line makes error because R is uppercase letter  
